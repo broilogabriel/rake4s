ccdc67d20841b3d588f72d97c1777624af74b08b
Gabriel Broilo <broilo.gabriel@gmail.com>
2017-09-01 01:51:52 +0100

    Working for the first example

---
+++ README.md
@@ -0,0 +1,3 @@
+# rake4s
+
+Scala implementation of RAKE (Rapid Automatic Keyword Extraction) based on Python RAKE from [aneesha/RAKE](https://github.com/aneesha/RAKE).
--- src/main/scala/com/broilogabriel/Rake.scala
+++ src/main/scala/com/broilogabriel/Rake.scala
@@ -6,9 +6,32 @@ object Rake {
 
   val delimiters = "[.!?,;:\t\\\\\"\\(\\)\\\'\u2019\u2013]|\\s\\-\\s"
 
-  def run(text: String) = {
-    text.split(delimiters).flatMap(s => s.split(stopsRegex)).filterNot(w => w.isEmpty || w == " ")
-    // TODO calculate_word_scores
+  def run(text: String): Map[String, Double] = {
+    val phraseList = text
+      .split(delimiters)
+      .flatMap(s => s.toLowerCase.split(stopsRegex).map(_.trim))
+      .filterNot(w => w.isEmpty || w == " ")
+    val scores = phraseList
+      .map(separateWords)
+      .flatMap(words => words.map((_, words.length - 1)))
+      .groupBy(_._1)
+      .map {
+        case (word, l) =>
+          val score = (l.length + l.map(_._2).sum) / (l.length * 1D)
+          word -> score
+      }
+    phraseList
+      .map(phrase => phrase -> separateWords(phrase).map(scores(_)).sum)
+      .toMap
+  }
+
+  private def separateWords(phrase: String): Array[String] = {
+    phrase.split("[^a-zA-Z0-9_\\+\\-/]")
+      .filter(w => w.nonEmpty && !w.matches("^\\d+$"))
+//      .map(_.toLowerCase.trim)
   }
 
 }
+
+
+
